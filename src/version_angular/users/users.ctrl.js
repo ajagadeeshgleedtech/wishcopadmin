4048ec5338fb1f214b2b3243123f80de77eba42e
Jagadeesh <ajagadeesh@gleedtech.com>
2017-04-26 12:36:09 +0530

    adding wishcop admin to repo

---
+++ src/version_angular/users/users.ctrl.js
@@ -0,0 +1,16 @@
+/**
+ * USERS LIST PAGE CONTROLLER
+ */
+
+app.controller('usersListCtrl',["$location", "$scope", "$rootScope","usersService",   function($location, $scope, $rootScope,usersServiceMethods   ) {
+
+ usersServiceMethods.getUsersList().then(function(response) {
+            $scope.userList = response.data;
+            // console.log($scope.userList);
+
+        });
+ 
+              
+ 
+
+}]);
